b7a5fb7bd6f6399fab401584c0d88828206a5bcd
RundesBalli <GitHub@RundesBalli.com>
2021-04-02 16:09:37 +0200

    Implemented HEX support, closes #1

--- noise.php
+++ noise.php
@@ -40,7 +40,8 @@ $introText.= "       https://github.com/RundesBalli/php-noise\n\n";
 $help = "Usage:\n";
 $help.= "All parameters are optional.\n";
 $help.= "-h, --help\n\tShows this help text and exits the script.\n";
-$help.= "-r <value>, -g <value>, -b <value>\n\tRed, green, blue\n\tPossible values: 0-255\n\tIf one of the parameters is invalid or not provided, it will be generated randomly.\n";
+$help.= "--hex <value>\n\tColor HEX Code\n\tPossible values: #000000-#FFFFFF\n\tThe hash (#) must not be provided. If the parameter is provided, the -r -g -b parameters will be ignored.\n\tIf the Hex-Code is invalid, a random color will be generated.\n";
+$help.= "-r <value>, -g <value>, -b <value>\n\tRed, green, blue\n\tPossible values: 0-255\n\tIf one of the parameters is invalid or not provided, it will be generated randomly.\n\tIf the --hex parameter is provided, all three of these parameters will be ignored.\n";
 $help.= "--tiles <value>\n\tNumber of tiles per row and column.\n\tThe image is square, therefore it hast \$tiles x \$tiles tiles.\n\tDefault: ".$tiles."\n\tIn CLI this value isn't capped. Outside of the CLI its capped to 50.\n";
 $help.= "--tileSize <value>\n\tWidth and height of one tile in pixels.\n\tDefault: ".$tileSize."\n\tIn CLI this value isn't capped. Outside of the CLI its capped to 20.\n";
 $help.= "--borderWidth <value>\n\tWidth of the grid which is drawed between tiles in pixels.\n\tDefault: ".$borderWidth."\n\tIn CLI this value isn't capped. Outside of the CLI its capped to 15.\n";
@@ -54,12 +55,53 @@ $help.= "\n";
  * 
  * @return array The validated or randomly generated colors.
  */
-function colorPicker(array $paramColors) {
+function colorPicker(array $paramColors = array()) {
   $arg = array();
   $arg['r'] = (((isset($paramColors['r']) AND is_numeric($paramColors['r'])) AND (intval($paramColors['r']) >= 0 AND intval($paramColors['r']) <= 255)) ? intval($paramColors['r']) : random_int(0, 255));
   $arg['g'] = (((isset($paramColors['g']) AND is_numeric($paramColors['g'])) AND (intval($paramColors['g']) >= 0 AND intval($paramColors['g']) <= 255)) ? intval($paramColors['g']) : random_int(0, 255));
   $arg['b'] = (((isset($paramColors['b']) AND is_numeric($paramColors['b'])) AND (intval($paramColors['b']) >= 0 AND intval($paramColors['b']) <= 255)) ? intval($paramColors['b']) : random_int(0, 255));
   return $arg;
+}
+
+/**
+ * hex2rgb function
+ * 
+ * Calculates a hex code (3 or 6 digit) to RGB.
+ * 
+ * @author    RundesBalli <webspam@rundesballi.com>
+ * @copyright 2020 RundesBalli
+ * @version   1.0
+ * @license   MIT-License
+ * @see       https://gist.github.com/RundesBalli/32f5491df25abb7fe0864e6447a26b75
+ * @see       https://www.php.net/manual/en/function.hexdec.php#99478
+ * @see       https://stackoverflow.com/questions/1636350/how-to-identify-a-given-string-is-hex-color-format/1637260#1637260
+ * 
+ * @param string $hex The hex string to be calculated.
+ * 
+ * @return array or boolean The RGB array or false.
+ */
+function hex2rgb($hex) {
+  if(preg_match("/^(?:(?:[0-9a-f]{2}){3}|(?:[0-9a-f]){3})$/i", preg_replace("/[^0-9a-f]/i", "", $hex), $result) === 1) {
+    if(strlen($result[0]) == 6) {
+      return array(
+        "r" => hexdec(substr($result[0], 0, 2)),
+        "g" => hexdec(substr($result[0], 2, 2)),
+        "b" => hexdec(substr($result[0], 4, 2)),
+        "hex" => $result[0]
+      );
+    } elseif(strlen($result[0]) == 3) {
+      return array(
+        "r" => hexdec(str_repeat(substr($result[0], 0, 1), 2)),
+        "g" => hexdec(str_repeat(substr($result[0], 1, 1), 2)),
+        "b" => hexdec(str_repeat(substr($result[0], 2, 1), 2)),
+        "hex" => $result[0]
+      );
+    } else  {
+      return FALSE;
+    }
+  } else {
+    return FALSE;
+  }
 }
 
 /**
@@ -75,7 +117,7 @@ if(php_sapi_name() == 'cli') {
   /**
    * Read arguments provided by CLI script call.
    */
-  $options = getopt("hr:g:b:", array("help", "tiles:", "tileSize:", "borderWidth:", "json"));
+  $options = getopt("hr:g:b:", array("help", "tiles:", "tileSize:", "borderWidth:", "json", "hex:"));
 
   /**
    * If the JSON parameter is provided via CLI, a note will be shown.
@@ -91,7 +133,21 @@ if(php_sapi_name() == 'cli') {
     die($help);
   }
 
-  $arg = colorPicker($options);
+  /**
+   * If a hex code is provided, use the hex code and ignore the rgb values.
+   */
+  if(isset($options['hex'])) {
+    $hex = hex2rgb($options['hex']);
+    if($hex) {
+      $arg = colorPicker($hex);
+      $filename = "noise_hex-".$hex['hex']."-t".$tiles."-tS".$tileSize."-bW".$borderWidth."_".md5(date("Y-m-d_H-i-s").microtime()).".png";
+    } else {
+      $arg = colorPicker();
+    }
+  } else {
+    $arg = colorPicker($options);
+  }
+
   $tiles = ((isset($options['tiles']) AND is_numeric($options['tiles'])) ? intval($options['tiles']) : $tiles);
   $tileSize = ((isset($options['tileSize']) AND is_numeric($options['tileSize'])) ? intval($options['tileSize']) : $tileSize);
   $borderWidth = ((isset($options['borderWidth']) AND is_numeric($options['borderWidth'])) ? intval($options['borderWidth']) : $borderWidth);
@@ -110,7 +166,21 @@ if(php_sapi_name() == 'cli') {
     die();
   }
   
-  $arg = colorPicker($_GET);
+  /**
+   * If a hex code is provided, use the hex code and ignore the rgb values.
+   */
+  if(isset($_GET['hex'])) {
+    $hex = hex2rgb($_GET['hex']);
+    if($hex) {
+      $arg = colorPicker($hex);
+      $filename = "noise_hex-".$hex['hex']."-t".$tiles."-tS".$tileSize."-bW".$borderWidth."_".md5(date("Y-m-d_H-i-s").microtime()).".png";
+    } else {
+      $arg = colorPicker();
+    }
+  } else {
+    $arg = colorPicker($_GET);
+  }
+
   $tiles = (((isset($_GET['tiles']) AND is_numeric($_GET['tiles'])) AND (intval($_GET['tiles']) > 0 AND intval($_GET['tiles']) <= 50)) ? intval($_GET['tiles']) : $tiles);
   $tileSize = (((isset($_GET['tileSize']) AND is_numeric($_GET['tileSize'])) AND (intval($_GET['tileSize']) > 0 AND intval($_GET['tileSize']) <= 20)) ? intval($_GET['tileSize']) : $tileSize);
   $borderWidth = (((isset($_GET['borderWidth']) AND is_numeric($_GET['borderWidth'])) AND (intval($_GET['borderWidth']) >= 0 AND intval($_GET['borderWidth']) <= 15)) ? intval($_GET['borderWidth']) : $borderWidth);
@@ -220,7 +290,12 @@ while($draw_x < $x) {
 /**
  * Save/Output the imagefile
  */
-$filename = "noise_r".$arg['r']."-g".$arg['g']."-b".$arg['b']."-t".$tiles."-tS".$tileSize."-bW".$borderWidth."_".md5(date("Y-m-d_H-i-s").microtime()).".png";
+if(empty($filename)) {
+  /**
+   * If a valid hex value is provided, the filename will be generated above.
+   */
+  $filename = "noise_r".$arg['r']."-g".$arg['g']."-b".$arg['b']."-t".$tiles."-tS".$tileSize."-bW".$borderWidth."_".md5(date("Y-m-d_H-i-s").microtime()).".png";
+}
 if($verbose == 1) {
   imagePNG($im, "./images/".$filename);
   echo "Output to:\n".realpath("./images/".$filename)."\n\n";
